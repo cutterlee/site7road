bff9c23120a43b06171da4168ecf4a8e7c00f93c
cutterlee <cutter.li.hn@gmail.com>
2014-10-11 19:20:01 +0800

    the newest database

--- doc/site7road/db_site_7road.sql
+++ doc/site7road/db_site_7road.sql
@@ -10,10 +10,54 @@ Target Server Type    : MYSQL
 Target Server Version : 50612
 File Encoding         : 65001
 
-Date: 2014-09-16 20:01:27
+Date: 2014-10-11 19:18:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `t_agent`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_agent`;
+CREATE TABLE `t_agent` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '跟业务无关的主键',
+  `order_num` int(11) DEFAULT '1' COMMENT '排序号',
+  `agent_name` varchar(50) NOT NULL COMMENT '代理商名称',
+  `agent_img` varchar(200) NOT NULL COMMENT '代理商的图片',
+  `agent_url` varchar(200) DEFAULT NULL COMMENT '代理商的主页链接',
+  `region_tag` varchar(50) NOT NULL COMMENT '所属区域',
+  `item_status` int(11) NOT NULL DEFAULT '1' COMMENT '数据的状态 1为正常 -1为删除',
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='代理商信息表';
+
+-- ----------------------------
+-- Records of t_agent
+-- ----------------------------
+INSERT INTO t_agent VALUES ('4', '1', 'xx', 'x', 'xx', 'xxx', '0', '2014-09-22 18:10:07');
+
+-- ----------------------------
+-- Table structure for `t_at_company`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_at_company`;
+CREATE TABLE `t_at_company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `order_num` int(11) DEFAULT NULL,
+  `item_status` int(11) NOT NULL,
+  `title_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content_summary` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '信息简介',
+  `content_info` text COLLATE utf8_unicode_ci,
+  `type_tag` varchar(50) COLLATE utf8_unicode_ci DEFAULT '0',
+  `small_image` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `big_image` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_q3t011lh9hcyfhsyi5fmbltvy` (`title_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of t_at_company
+-- ----------------------------
+
 -- ----------------------------
 -- Table structure for `t_company_info`
 -- ----------------------------
@@ -35,6 +79,110 @@ INSERT INTO t_company_info VALUES ('1', '第七大道', '可信科学院', '0755
 INSERT INTO t_company_info VALUES ('11', '第八大道', '科兴科学园A栋', '07557987988', '真诚,创新,成果', '真诚,创新,成果');
 INSERT INTO t_company_info VALUES ('12', '第九大道', '科兴科学园', '07557987989', '真诚,创新,成果', '真诚,创新,成果');
 INSERT INTO t_company_info VALUES ('13', '第十大道', '科兴科学园', '07557987990', '真诚,创新,成果xxxxxxx的对的对的对的对的对的对的对的对的对的对的对的对的对的对的对的对的对的对的对的', '真诚,创新,成果');
+
+-- ----------------------------
+-- Table structure for `t_config`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_config`;
+CREATE TABLE `t_config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `order_num` int(11) DEFAULT NULL,
+  `item_status` int(11) NOT NULL,
+  `key_tag` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `text_content` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pid` int(11) DEFAULT '0',
+  `description` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `img_words` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `english_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `img_url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '图片',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_q3t011lh9hcyfhsyi5fmbltvy` (`key_tag`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of t_config
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_event`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_event`;
+CREATE TABLE `t_event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_num` int(11) DEFAULT '1' COMMENT '排序号',
+  `event_title` varchar(50) NOT NULL COMMENT '事件的标题',
+  `publish_time` date NOT NULL COMMENT '事件发生的日期',
+  `event_content_summary` longtext NOT NULL COMMENT '事件简介',
+  `event_content_detail` longtext NOT NULL COMMENT '事件详细',
+  `event_html_name` varchar(20) NOT NULL COMMENT '事件的html名称',
+  `event_img_in_event_page` varchar(200) NOT NULL COMMENT '事件在列表页的图片',
+  `item_status` int(11) NOT NULL DEFAULT '1' COMMENT '数据状态 1为正常 -1为删除',
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_title` (`event_title`),
+  UNIQUE KEY `unique_html_name` (`event_html_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='事件信息表';
+
+-- ----------------------------
+-- Records of t_event
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_job`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_job`;
+CREATE TABLE `t_job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '跟业务无关的主键',
+  `order_num` int(11) DEFAULT '1' COMMENT '排序号',
+  `position_name` varchar(50) DEFAULT NULL COMMENT '职位名称',
+  `position_need_num` int(11) NOT NULL COMMENT '所需人数',
+  `position_place` varchar(20) DEFAULT NULL COMMENT '所在城市',
+  `position_property` varchar(50) DEFAULT NULL COMMENT '职位性质',
+  `position_duty` longtext COMMENT '职位职责',
+  `position_requirement` longtext COMMENT '职位要求',
+  `is_hot_job` tinyint(1) DEFAULT '0' COMMENT '是否热招职位 0标识不是 1标识热招',
+  `item_status` int(11) NOT NULL DEFAULT '1' COMMENT '数据的状态 1 可用 -1 删除',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_mrmoe48dhh7xbmxl4gyvi33t3` (`position_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='招聘职位表';
+
+-- ----------------------------
+-- Records of t_job
+-- ----------------------------
+INSERT INTO t_job VALUES ('3', '1', 'java开发工程师', '10', '深圳', '技术', 'java开发工程师java开发工程师java开发工程师', 'java开发工程师java开发工程师java开发工程师java开发工程师', '1', '0', '2014-09-19 17:15:46');
+
+-- ----------------------------
+-- Table structure for `t_product`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_product`;
+CREATE TABLE `t_product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_num` int(11) DEFAULT '1' COMMENT '顺序号',
+  `pro_type` int(11) NOT NULL DEFAULT '0' COMMENT '产品类型',
+  `pro_name` varchar(20) DEFAULT NULL COMMENT '产品名称',
+  `pro_title` varchar(100) NOT NULL COMMENT '产品标题',
+  `pro_website_url` varchar(100) DEFAULT NULL COMMENT '产品站点链接',
+  `pro_play_url` varchar(200) DEFAULT NULL COMMENT '产品使用链接',
+  `pro_html_name` varchar(20) DEFAULT NULL COMMENT '产品的html名称',
+  `pro_img_in_product` varchar(200) DEFAULT NULL COMMENT '产品页的图片链接',
+  `pro_short_desc` varchar(2000) DEFAULT NULL COMMENT '产品简介',
+  `pro_detail_desc` longtext COMMENT '产品详细信息',
+  `index_img_url` varchar(200) DEFAULT NULL COMMENT '首页图片',
+  `index_img_link` varchar(200) DEFAULT NULL COMMENT '首页轮播的图片链接',
+  `index_game_short` varchar(100) DEFAULT NULL COMMENT '首页轮播的简单文字',
+  `index_game_detail` varchar(200) DEFAULT NULL COMMENT '首页轮播的详细文字',
+  `index_order` int(11) DEFAULT '1' COMMENT '首页轮播的排序号',
+  `update_time` datetime DEFAULT '2013-01-01 00:00:00' COMMENT '数据修改时间',
+  `item_status` int(11) DEFAULT '1' COMMENT '数据的状态1 正常  -1删除',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_4lf9gkwiteqhjqqr8ak8id5m3` (`pro_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 COMMENT='产品信息表';
+
+-- ----------------------------
+-- Records of t_product
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `t_resource_info`
@@ -143,84 +291,11 @@ CREATE TABLE `t_user_info` (
   `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
   `login_times` int(11) DEFAULT NULL COMMENT '登录次数',
   `user_status` tinyint(1) DEFAULT '2' COMMENT ' 用户的状态 1 审核通过  2 暂未审核  3冻结 ',
-  `role_set_str` text COMMENT '角色id集合字段',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色id集合字段',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
 -- Records of t_user_info
 -- ----------------------------
-INSERT INTO t_user_info VALUES ('3', 'anyelangwang', 'aaaaaaaaaaaaaa', null, '李福春', 'cutter.li@7road.com', '2014-09-15 00:00:00', '0:0:0:0:0:0:0:1', '2014-09-15 09:31:08', '0', '1', null);
-
-DROP TABLE IF EXISTS `t_job`;
-CREATE TABLE `t_job` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '跟业务无关的主键',
-  `order_num` int(11) DEFAULT '1' COMMENT '排序号',
-  `position_name` varchar(50) DEFAULT NULL COMMENT '职位名称',
-  `position_need_num` int(11) NOT NULL COMMENT '所需人数',
-  `position_place` varchar(20) DEFAULT NULL COMMENT '所在城市',
-  `position_property` varchar(50) DEFAULT NULL COMMENT '职位性质',
-  `position_duty` longtext COMMENT '职位职责',
-  `position_requirement` longtext COMMENT '职位要求',
-  `is_hot_job` tinyint(1) DEFAULT '0' COMMENT '是否热招职位 0标识不是 1标识热招',
-  `item_status` int(11) NOT NULL DEFAULT '1' COMMENT '数据的状态 1 可用 -1 删除',
-  `create_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_mrmoe48dhh7xbmxl4gyvi33t3` (`position_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='招聘职位表'
-
-
-DROP TABLE IF EXISTS `t_product`;
-CREATE TABLE `t_product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_num` int(11) DEFAULT '1' COMMENT '顺序号',
-  `pro_type` int(11) NOT NULL DEFAULT '0' COMMENT '产品类型',
-  `pro_name` varchar(20) DEFAULT NULL COMMENT '产品名称',
-  `pro_title` varchar(100) NOT NULL COMMENT '产品标题',
-  `pro_website_url` varchar(100) DEFAULT NULL COMMENT '产品站点链接',
-  `pro_play_url` varchar(200) DEFAULT NULL COMMENT '产品使用链接',
-  `pro_html_name` varchar(20) DEFAULT NULL COMMENT '产品的html名称',
-  `pro_img_in_product` varchar(200) DEFAULT NULL COMMENT '产品页的图片链接',
-  `pro_short_desc` varchar(2000) DEFAULT NULL COMMENT '产品简介',
-  `pro_detail_desc` longtext COMMENT '产品详细信息',
-  `index_img_url` varchar(200) DEFAULT NULL COMMENT '首页图片',
-  `index_img_link` varchar(200) DEFAULT NULL COMMENT '首页轮播的图片链接',
-  `index_game_short` varchar(100) DEFAULT NULL COMMENT '首页轮播的简单文字',
-  `index_game_detail` varchar(200) DEFAULT NULL COMMENT '首页轮播的详细文字',
-  `index_order` int(11) DEFAULT '1' COMMENT '首页轮播的排序号',
-  `update_time` datetime DEFAULT '2013-01-01 00:00:00' COMMENT '数据修改时间',
-  `item_status` int(11) DEFAULT '1' COMMENT '数据的状态1 正常  -1删除',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_4lf9gkwiteqhjqqr8ak8id5m3` (`pro_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 COMMENT='产品信息表'
-
-DROP TABLE IF EXISTS `t_agent`;
-CREATE TABLE `t_agent` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '跟业务无关的主键',
-  `order_num` int(11) DEFAULT '1' COMMENT '排序号',
-  `agent_name` varchar(50) NOT NULL COMMENT '代理商名称',
-  `agent_img` varchar(200) NOT NULL COMMENT '代理商的图片',
-  `agent_url` varchar(200) DEFAULT NULL COMMENT '代理商的主页链接',
-  `region_tag` varchar(50) NOT NULL COMMENT '所属区域',
-  `item_status` int(11) NOT NULL DEFAULT '1' COMMENT '数据的状态 1为正常 -1为删除',
-  `create_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='代理商信息表'
-
-
-DROP TABLE IF EXISTS `t_event`;
-CREATE TABLE `t_event` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_num` int(11) DEFAULT '1' COMMENT '排序号',
-  `event_title` varchar(50) NOT NULL COMMENT '事件的标题',
-  `publish_time` date NOT NULL COMMENT '事件发生的日期',
-  `event_content_summary` longtext NOT NULL COMMENT '事件简介',
-  `event_content_detail` longtext NOT NULL COMMENT '事件详细',
-  `event_html_name` varchar(20) NOT NULL COMMENT '事件的html名称',
-  `event_img_in_event_page` varchar(200) NOT NULL COMMENT '事件在列表页的图片',
-  `item_status` int(11) NOT NULL DEFAULT '1' COMMENT '数据状态 1为正常 -1为删除',
-  `update_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_title` (`event_title`),
-  UNIQUE KEY `unique_html_name` (`event_html_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='事件信息表'
+INSERT INTO t_user_info VALUES ('3', 'anyelangwang', 'abcabc', null, '李福春', 'cutter.li@7road.com', '2014-09-15 00:00:00', '0:0:0:0:0:0:0:1', '2014-09-15 09:31:08', '0', '1', '1');
